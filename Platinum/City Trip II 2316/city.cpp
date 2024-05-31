3d3fb9e57d42cabb925f8ff997a8c640d4677e12
RoelYoon <roelyoon2@gmail.com>
2024-05-31 20:16:34 +0900

    Removed [UNSOLVED] status

--- Platinum/City Trip II 2316/city.cpp
+++ Platinum/City Trip II 2316/city.cpp
@@ -1,126 +1,94 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#define INF 1000000000
 using namespace std;
 class FlowNetwork{
     private:
     #define SS 0
     #define ST 1
-    #define IN(i) (i)
-    #define OUT(i) (i+V)
-    #define INF 1000000000
+    #define OUT(i) ((i)+n)
     struct Edge{
         int u;
         int v;
         int cap;
         int flow;
         Edge(){}
-        Edge(int u, int v, int c, int f):u(v),v(v),cap(c),flow(f){}
+        Edge(int u, int v, int c, int f):u(u),v(v),cap(c),flow(f){}
     };
-    int V, GV;
+    int n;
+    int V;
     vector<Edge> edges;
     vector<vector<int>> graph;
-    vector<int> level;
-    vector<int> savedPointDFS;
+    vector<int> levels;
+    vector<int> saveDFS;
     bool BFS(){
-        queue<int> q({0});
-        level.assign(V,-1);
-        level[SS]=0;
+        queue<int> q({SS});
+        levels.assign(V,-1);
+        levels[SS]=0;
         while(!q.empty()){
             int cur = q.front(); q.pop();
             if(cur==ST) break;
-            for(int i = 0; i < graph[cur].size(); i++){
+            for(int i = 0; i < graph[cur].size(); ++i){
                 Edge &edge = edges[graph[cur][i]];
-                if(edge.cap-edge.flow > 0 && level[edge.v]==-1){
-                    level[edge.v]=level[cur]+1;
+                if(edge.cap - edge.flow > 0 && levels[edge.v]==-1){
+                    levels[edge.v]=levels[cur]+1;
                     q.push(edge.v);
                 }
             }
         }
-        return level[ST]!=-1;
+        return levels[ST]!=-1;
     }
     int DFS(int cur, int f){
         if(cur==ST || f==0) return f;
-        for(int &i = savedPointDFS[cur]; i < graph[cur].size(); i++){
+        for(int &i = saveDFS[cur]; i < graph[cur].size(); ++i){
             Edge &edge = edges[graph[cur][i]];
-            if(level[edge.v]!=level[cur]+1) continue;
-            int pushedFlow = DFS(edge.v,min(f,edge.cap-edge.flow));
-            if(pushedFlow){
-                edge.flow += pushedFlow;
+            if(levels[edge.v]!=levels[cur]+1) continue;
+            if(int pushedFlow = DFS(edge.v,min(f,edge.cap-edge.flow))){
+                edge.flow+=pushedFlow;
                 Edge &rEdge = edges[graph[cur][i]^1];
-                rEdge.flow -= pushedFlow;
+                rEdge.flow-=pushedFlow;
                 return pushedFlow;
             }
         }
         return 0;
     }
     public:
-    FlowNetwork(int v):V(v),GV(2*V),graph(GV),level(GV){
-        edges.emplace_back(IN(0),OUT(0),INF,0);
-        graph[IN(0)].push_back(edges.size()-1);
-        edges.emplace_back(OUT(0),IN(0),INF,0);
-        graph[OUT(0)].push_back(edges.size()-1);
-
-        edges.emplace_back(IN(1),OUT(1),INF,0);
-        graph[IN(1)].push_back(edges.size()-1);
-        edges.emplace_back(OUT(1),IN(1),INF,0);
-        graph[OUT(1)].push_back(edges.size()-1);
-        for(int i = 2; i < V; ++i){
-            edges.emplace_back(IN(i),OUT(i),1,0);
-            graph[IN(i)].push_back(edges.size()-1);
-            edges.emplace_back(OUT(i),IN(i),1,0);
+    FlowNetwork(int n):n(n),V(2*n),graph(V){
+        for(int i = 0; i < n; ++i){
+            edges.emplace_back(i,OUT(i),i==SS || i==ST ? INF : 1,0);
+            graph[i].push_back(edges.size()-1);
+            edges.emplace_back(OUT(i),i,0,0);
             graph[OUT(i)].push_back(edges.size()-1);
-        }
-    }
-    void addEdge(int u, int v, int c){
-        // in-node
-        edges.emplace_back(OUT(u),IN(v),c,0);
-        graph[OUT(u)].push_back(edges.size()-1);
-        edges.emplace_back(IN(v),OUT(u),c,0);
-        graph[IN(v)].push_back(edges.size()-1);
+        }   
     }
     int maxFlow(){
         int mf = 0;
         while(BFS()){
-            savedPointDFS.assign(GV,0);
+            saveDFS.assign(V,0);
             while(int f = DFS(SS,INF)){
                 mf+=f;
             }
         }
         return mf;
     }
+    void addEdge(int u, int v, int c){
+        static bool forward = 1;
+        edges.emplace_back(OUT(u),v,c,0);
+        graph[OUT(u)].push_back(edges.size()-1);
+        edges.emplace_back(v,OUT(u),0,0);
+        graph[v].push_back(edges.size()-1);
+        forward = !forward;
+        if(forward){return;}
+        addEdge(v,u,c);
+    }
 };
 int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n,p; cin>>n>>p;
+    int n, p; cin>>n>>p;
     FlowNetwork fn(n);
     for(int i = 0; i < p; ++i){
-        int u,v; cin>>u>>v; --u, --v;
+        int u, v; cin>>u>>v; --u, --v;
         fn.addEdge(u,v,INF);
     }
     cout<<fn.maxFlow()<<"\n";
 }
-/*
-8 9
-1 3
-3 4
-4 2
-2 6
-6 5
-5 3
-1 7
-7 8
-8 4
-2
-
-6 7
-3 1
-3 4
-4 2
-1 6
-3 5
-5 2
-3 6
-1
-*/
