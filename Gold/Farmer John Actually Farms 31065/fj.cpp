38af4d5527a5340a5084bdb9055eb95e903cbf18
Roel Yoon <roelyoon2@gmail.com>
2024-11-16 19:21:53 +0900

    Shorten 31065

--- Gold/Farmer John Actually Farms 31065/fj.cpp
+++ Gold/Farmer John Actually Farms 31065/fj.cpp
@@ -7,16 +7,6 @@ struct Plant{
     ll h,a,t;
     Plant(){}
 };
-bool passes(vector<Plant> &p, ll ans){
-    if(ans<0) return 0;
-    sort(p.begin(),p.end(),[ans](const Plant &a, const Plant &b){
-        return a.h+a.a*ans>=b.h+b.a*ans;
-    });
-    for(int i = 0; i < p.size(); ++i)
-        if(p[i].t!=i)
-            return 0;
-    return 1;
-}
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -31,9 +21,8 @@ int main(){
         for(int i = 0; i < n; ++i)
             cin>>p[i].t;
         sort(p.begin(),p.end(),[](const Plant &a, const Plant &b){return a.t<b.t;});
-        ll minD=0,maxD=9e18;
+        ll minD=0,maxD=1e10;
         for(int i = n-1; i > 0; --i){
-            //a wants to be taller than b
             Plant &a = p[i-1];
             Plant &b = p[i];
             if(a.h<=b.h){
@@ -45,22 +34,18 @@ int main(){
                     minD+=!(a.h+a.a*minD > b.h+b.a*minD);
                 }
             }else{
-                //how long can a maintain being taller than b?
                 if(a.a<b.a){
                     maxD=min(maxD,(a.h-b.h)/(b.a-a.a));
+                    maxD-=a.h+a.a*maxD==b.h+b.a*maxD;
                 }
             }
         }
-        cout<<(minD<=maxD && passes(p,minD)?minD:-1)<<"\n";
+        cout<<(minD<=maxD?minD:-1)<<"\n";
     }
 }
 /*
 1
 3
-4 8 5
-3 1 3
-2 1 0
-
 5 8 4
 3 1 3
 0 1 2
