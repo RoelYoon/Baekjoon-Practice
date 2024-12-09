390e89eb601319c0c7fd5b86eeeee7f74f20ab33
Roel Yoon <roelyoon2@gmail.com>
2024-12-09 18:55:51 +0900

    Solve 10882

--- Silver/International Meeting 10882/meeting.cpp
+++ Silver/International Meeting 10882/meeting.cpp
@@ -13,14 +13,5 @@ int main(){
         int time = UTC+(UTCs[3]=='+'?round(stold(UTCs.substr(4))*60):-round(stold(UTCs.substr(4))*60));
         time=(time+1440)%1440;
         cout<<(!((time/60)/10)?"0":"")<<time/60<<":"<<((!((time%60)/10)?"0":""))<<time%60<<"\n";
-        //cout<<time<<"\n";
     }    
 }
-/*
-4
-00:00 UTC+0
-UTC+9.5
-UTC-1
-UTC+12
-UTC+10
-*/
