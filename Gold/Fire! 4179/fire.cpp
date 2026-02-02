57d4408c67317294cdb8284ec444dbd6b3eb0124
Roel Yoon <roelyoon2@gmail.com>
2026-02-01 19:52:48 -0500

    Solve 4179

---
+++ Gold/Fire! 4179/fire.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#define INF ((int)1e9)
+using namespace std;
+const int dirR[4]{0,1,0,-1};
+const int dirC[4]{1,0,-1,0};
+int R,C;
+struct Pos{
+    int r,c,t;
+    Pos(){}
+    Pos(int r, int c, int t):r(r),c(c),t(t){}
+};
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>R>>C;
+    vector<string> grid(R);
+    Pos J,F;
+    queue<Pos> q;
+    vector<vector<int>> time(R,vector<int>(C,INF));
+    q.push(F);
+    for(int r = 0; r < R; ++r){
+        string s; cin>>s;
+        for(int c= 0; c < C; ++c){
+            grid[r]=s;
+            if(s[c]=='J')
+                J = Pos(r,c,0);
+            else if (s[c]=='F'){
+                q.emplace(r,c,0);
+                time[r][c] = 0;
+            }
+        }
+    }
+    while(!q.empty()){
+        Pos cur = q.front(); q.pop();
+        for(int i = 0; i < 4; ++i){
+            int nr = cur.r+dirR[i];
+            int nc = cur.c+dirC[i];
+            if(nr>=0 && nr<R && nc>=0 && nc<C && time[nr][nc]>cur.t+1 && grid[nr][nc]!='#'){
+                q.emplace(nr,nc,cur.t+1);
+                time[nr][nc] = cur.t+1;
+            }
+        }
+    }
+    q.push(J);
+    vector<vector<int>> JTime(R,vector<int>(C,INF));
+    bool possible =0;
+    int ans = -1;
+    JTime[J.r][J.c]=0;
+    while(!q.empty()){
+        Pos cur = q.front(); q.pop();
+        if(cur.r==0 || cur.c==0 || cur.r==R-1 || cur.c == C-1){
+            possible=1;
+            ans = cur.t;
+            break;
+        }
+        for(int i = 0; i < 4; ++i){
+            int nr = cur.r+dirR[i];
+            int nc = cur.c+dirC[i];
+            if(nr>=0 && nr<R && nc>=0 && nc<C && time[nr][nc]>cur.t+1 && JTime[nr][nc]>cur.t+1 && grid[nr][nc]!='#'){
+                q.emplace(nr,nc,cur.t+1);
+                JTime[nr][nc] = cur.t+1;
+            }
+        }
+    }
+    if(possible){
+        cout<<ans+1<<"\n";
+    }else{
+        cout<<"IMPOSSIBLE\n";
+    }
+}
