e4c779f01d72cd1288654d28b6905a59f454dd45
Roel Yoon <roelyoon2@gmail.com>
2026-02-28 16:13:15 -0500

    Solve 14003

--- Platinum/LIS V 14003/lis.cpp
+++ Platinum/LIS V 14003/lis.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
-#define INF ((int)1e9)
+#define INF ((int)1e9+1)
 #define MAX_N 1000000
 using namespace std;
 int DP[MAX_N+1];
-void printPath(int p[], int nums[], int a){
-    if(p[a] == a){
-        cout<<nums[a];
+int ind[MAX_N+1];
+int p[MAX_N];
+int nums[MAX_N];
+void printPath(int i){
+    if(p[i] == -1){
+        cout<<nums[i];
         return;
     }
-    printPath(p, nums, p[a]);
-    cout<<" "<<nums[a];
+    printPath(p[i]);
+    cout<<" "<<nums[i];
 }
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     int n; cin>>n;
-    int nums[MAX_N];
     for(int i = 0; i < n; ++i){
         cin>>nums[i];
         DP[i+1] = INF;
     }
     DP[0] = -INF;
-    int ind[MAX_N+1];
-    int p[MAX_N];
+    ind[0] = -1;
     for(int i = 0; i < n; ++i){
         int lb = 0, rb = n;
         while(lb < rb){
@@ -33,22 +34,20 @@ int main(){
                 rb = mid;
             }
         }
-        if(lb==0 || DP[lb]==nums[i]) continue;
         if(nums[i] < DP[lb]){
             DP[lb] = nums[i];
             ind[lb] = i;
-            p[i] = lb-1==0?i:ind[lb-1];
+            p[i] = ind[lb-1];
         }
     }
     int ans = 0;
-    int e = -1;
-    for(int i = 1; i <= n; ++i){
+    for(int i = n; i >= 1; --i){
         if(DP[i]!=INF){
             ans = i;
-            e = ind[i];
+            break;
         }
     }
     cout<<ans<<"\n";
-    printPath(p, nums, e);
+    printPath(ind[ans]);
     cout<<"\n";
 }
