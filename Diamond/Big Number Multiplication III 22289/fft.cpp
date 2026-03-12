61b211c891c1621b71c654e26268abb8655a91f7
Roel Yoon <roelyoon2@gmail.com>
2026-03-11 20:10:02 -0400

    Solve 22289

---
+++ Diamond/Big Number Multiplication III 22289/fft.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <complex>
+#include <algorithm>
+#define cd complex<double>
+using namespace std;
+const double PI = acos(-1);
+int reverse(int num, int lg_n) {
+    int res = 0;
+    for (int i = 0; i < lg_n; i++) {
+        if (num & (1 << i))
+            res |= 1 << (lg_n - 1 - i);
+    }
+    return res;
+}
+void fft(vector<cd> & a, bool invert) {
+    int n = a.size();
+    int lg_n = 0;
+    while ((1 << lg_n) < n)
+        lg_n++;
+
+    for (int i = 0; i < n; i++) {
+        if (i < reverse(i, lg_n))
+            swap(a[i], a[reverse(i, lg_n)]);
+    }
+
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = 2 * PI / len * (invert ? -1 : 1);
+        cd wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            cd w(1);
+            for (int j = 0; j < len / 2; j++) {
+                cd u = a[i+j], v = a[i+j+len/2] * w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w *= wlen;
+            }
+        }
+    }
+
+    if (invert) {
+        for (cd & x : a)
+            x /= n;
+    }
+}
+vector<int> multiply(vector<int> const& a, vector<int> const& b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<int> result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    int carry = 0;
+    for (int i = 0; i < n; i++) {
+        result[i] += carry;
+        carry = result[i] / 10;
+        result[i] %= 10;
+    }
+    return result;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string a,b; cin>>a>>b;
+    vector<int> av(a.size()),bv(b.size());
+    for(int i = 0; i < a.size(); ++i){
+        av[a.size()-1-i] = a[i]-'0';
+    }
+    for(int i = 0; i < b.size(); ++i){
+        bv[b.size()-1-i] = b[i]-'0';
+    }
+    vector<int> m = multiply(av,bv);
+    bool f = 0;
+    for(int i = m.size()-1; i >= 0; --i){
+        if(!f && !m[i]) continue;
+        f = 1;
+        cout<<m[i];
+    }
+    if(!f) cout<<"0";
+    cout<<"\n";
+}
