a3aad485cff8b6232db9d0021b277528341abb9d
Roel Yoon <roelyoon2@gmail.com>
2026-03-29 15:41:18 -0400

    Solve 14681

---
+++ Bronze/Quadrant Selection 14681/quad.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a,b; cin>>a>>b;
+    if(a>0 && b>0){
+        cout<<"1\n";
+    }else if(a>0 && b<0){
+        cout<<"4\n";
+    }else if(a<0 && b<0){
+        cout<<"3\n";
+    }else if(a<0 && b>0){
+        cout<<"2\n";
+    }
+}
